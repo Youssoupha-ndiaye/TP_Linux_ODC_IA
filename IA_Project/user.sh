5aadcbdba7d5689f3d836a4460218bf1928cb3e9
youssoupha-ndiaye <ndiayeyoussoupha264@gmail.com>
2026-07-28 08:25:08 +0000

    script create user

---
+++ IA_Project/user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "creation du premier user"
+
+sudo adduser "alice"
+
+echo "creation du deuxieme user"
+
+sudo adduser "bob"
+
+echo "creation du troisiéme user"
+
+sudo adduser "charles"
+
+echo "creation du quatriéme user"
+
+sudo adduser "diane"
+
+echo "creation du cinquiéme user"
+
+sudo adduser "eva"
